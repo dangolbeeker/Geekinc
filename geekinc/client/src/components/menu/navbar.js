57c9790d3b64e3bc958d7e919362131168ee4379
dangolbeeker <scottb305@outlook.com>
2019-12-01 20:47:58 -0500

    playing with nav and structure of app

---
+++ geekinc/client/src/components/menu/navbar.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.scss';
+
+export default class Navbar extends Component {
+
+  /** @param {{links: {to: string, text: string, isRoute: boolean, html: JSX.Element}[]}} props */
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      links: props.links
+    };
+  }
+
+  /** @param {React.MouseEvent<HTMLDivElement>} e */
+  slideNav = (e) => {
+    const burger = document.querySelector("#burger");
+    const links = document.querySelector("#links");
+    const allLinks = document.querySelectorAll("#links li");
+
+    links.classList.toggle('show-links');
+    burger.classList.toggle('show-links');
+
+    allLinks.forEach((link) => {
+      link.classList.toggle('scale-fade-animation');
+    });
+  };
+
+  render() {
+    const { links } = this.state;
+    return (
+      <div id="navbar">
+        <h1>Logo</h1>
+
+        <ul id="links">
+          {/* <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Sign In</li> */}
+          {
+            links.map((link) => {
+              if (link.html) return link.html;
+
+              if (link.isRoute) return (
+                <li><Link to={link.to}>{link.text} </Link> </li>
+              )
+
+              return (
+                <li><a href={link.to}>{link.text}</a></li>
+              );
+            })
+          }
+        </ul>
+
+        <div id="burger" onClick={this.slideNav}>
+          <div className="line1"></div>
+          <div className="line2"></div>
+          <div className="line3"></div>
+        </div>
+      </div>
+    )
+  }
+}
